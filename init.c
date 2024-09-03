e954ae6ed7275292b49ae6aabc0546d84806f12e
EduardoHGomez <hurtado.josegomez@gmail.com>
2024-09-02 19:21:59 -0600

    step

--- init.c
+++ init.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <string.h>
 
 int num_children = 6;
+int processes[6];
 
-void create_getty(){
-
-    
-    
+// Function to create getty process in xterm
+void create_getty(int index) {
+    int pid = fork();
+    if (pid == 0) {
+        processes[index] = pid;
+        execlp("xterm", "xterm", "-e", "./getty", (char *)NULL);
+        perror("Failed to exec getty");
+        exit(1);
+    }
 }
 
-int main(){
-        
-    for(int i =0; i<6; i++){
-        if(fork()==0){
-            execlp("xterm","xterm","-e"," ./getty",(char *)NULL);
-
-        }
-        
+int main() {
+    // Create 6 getty processes
+    for (int i = 0; i < num_children; i++) {
+        create_getty(i);
     }
+
+
+    return 0;
 }
